8472a8bb8299e734828212efbedc66389fd5b014
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-08-24 00:33:48 +0200

    Added search method

--- src/main/java/pl/katarzynawojtowicz/BudgetPlanner/profit/ProfitRestController.java
+++ src/main/java/pl/katarzynawojtowicz/BudgetPlanner/profit/ProfitRestController.java
@@ -1,5 +1,7 @@
 package pl.katarzynawojtowicz.BudgetPlanner.profit;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -18,5 +21,12 @@ public class ProfitRestController {
 	public ResponseEntity<Object> addNewProfit(@RequestBody ProfitTo newProfit) {
 		ProfitDao.addNewProfit(newProfit);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ProfitTo>> getFindByStatus(
+			@RequestParam(value = "nazwa", required = false) String nazwa) {
+		List<ProfitTo> searchByStatus = ProfitDao.findByParameters(nazwa);
+		return new ResponseEntity<>(searchByStatus, HttpStatus.OK);
 	}
 }
