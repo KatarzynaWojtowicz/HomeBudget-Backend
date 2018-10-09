6b73b6cb682c6c6ff57e6c9c1277c5614b677007
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-10-09 20:48:22 +0200

    Created user_data.sql and user_install.sql

---
+++ sql/0.0.1/user_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user(login, password) VALUES ('kasia','haslo123');
+INSERT INTO user(login, password) VALUES ('madzia','haslo321');
---
+++ sql/0.0.1/user_install.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `homebudget`.`user` (
+  `id` INT NOT NULL,
+  `login` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
