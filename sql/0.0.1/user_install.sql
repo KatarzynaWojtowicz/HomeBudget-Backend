5666fcfec98fcce04a19a881c039f5be42c3d005
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-10-10 14:03:17 +0200

    Added new column i user table with email and changed value of varchar in login and password

--- sql/0.0.1/user_install.sql
+++ sql/0.0.1/user_install.sql
@@ -1,5 +1,8 @@
 CREATE TABLE `homebudget`.`user` (
   `id` INT NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `login` VARCHAR(64) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `email` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`id`));
+  
+  
